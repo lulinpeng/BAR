6b111b6b3193197cf694d60b1d2b148e42f043ab
lulinpeng <turingcrypt@gmail.com>
2025-07-29 11:06:31 +0800

    add examples of rotating

--- manim-python/rotate_about_x.py
+++ manim-python/rotate_about_x.py
@@ -1,12 +1,33 @@
 from manim import *
 
-class RotateAboutX(Scene):
+class RotateDotAboutX(Scene):
     def construct(self):
-        pos = [2.0, 0.0, 0.0]
+        pos = [2, 0, 0]
         dot = Dot(pos)
         self.play(Create(dot))
         x = [0, 0, 0]
-        # rotate 'dot' about the base point 'x'
-        for _ in range(10):
+        for _ in range(10): # rotate 'dot' about the base point 'x'
             self.wait(1)
             self.play(Rotating(dot, about_point= x, radians=1, run_time=2))
+
+class RotateLineAboutX(Scene):
+    def construct(self):
+        pos = [0, 0, 0]
+        line = Line(pos)
+        self.play(Create(line))
+        x = [0, 0, 0]
+        for _ in range(10): # rotate 'line' about the base point 'x'
+            self.wait(1)
+            self.play(Rotating(line, about_point=x, radians=1, run_time=2))
+            
+class RotateAllAboutX(Scene):
+    def construct(self):
+        pos = [0, 0, 0]
+        line = Line(pos)
+        pos = [2, 0, 0]
+        dot = Dot(pos)
+        self.play(Create(line), Create(dot))
+        x = [0, 0, 0]
+        for _ in range(10): # rotate 'line' and 'dot' about the base point 'x'
+            self.wait(1)
+            self.play(Rotating(line, about_point=x, radians=1, run_time=2), Rotating(dot, about_point=x, radians=1, run_time=2))
