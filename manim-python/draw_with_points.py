84f929b5fff71a47a23586c8390fe3d24abc007d
lulinpeng <turingcrypt@gmail.com>
2025-07-29 11:58:01 +0800

    draw curve with points

--- manim-python/draw_with_points.py
+++ manim-python/draw_with_points.py
@@ -12,3 +12,13 @@ class DrawCircle(Scene):
             points.append(np.array([x, y, 0]))
         vmo.points = points
         self.add(plane, vmo)
+
+class DrawCurve(Scene):
+    def construct(self):
+        plane = NumberPlane()
+        vmo = VMobject(color=GREEN)
+        sub_curve0 = [np.array([-2, -1, 0]), np.array([-3, 1, 0]), np.array([0, 3, 0]), np.array([1, 3, 0])]
+        sub_curve1 = [np.array([1,  3,  0]), np.array([ 0, 1, 0]), np.array([4, 3, 0]), np.array([4, -2, 0])]
+        curve_points = sub_curve0 + sub_curve1
+        vmo.points = curve_points
+        self.add(plane, vmo)
